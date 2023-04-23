d0b9c17755c3be1aebc0c08d412c0e2ba6ba3958
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-23 17:04:23 +0200

    Add files via upload

---
+++ src/routes/chatbots.svelte
@@ -0,0 +1,70 @@
+<!-- src/routes/chatbots.svelte -->
+<svelte:head>
+	<title>Chatbots</title>
+</svelte:head>
+
+
+<script context="module">
+    // Update the fetch URL with your chatbot data API or use the same JSONPlaceholder API
+    export const load = async ({ fetch }) => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const chatbots = await res.json();
+        return {
+            props: {
+                chatbots,
+            }
+        }
+    }
+ 
+</script>
+
+<script>
+    export let chatbots; // update the variable name
+</script>
+
+<div class="container">
+    <h1>My Chatbots</h1> 
+
+    <div class="chatbots">
+        {#each chatbots as chatbot} <!-- Update the variable name -->
+        <div class="chatbot">
+            <h2>{chatbot.title.substring(0, 20)}</h2>
+            <p>{chatbot.body.substring(0, 80)}</p>
+            <p class="readmore"><a style=" color: rgb(10, 10, 139);" href={`/chatbots/${chatbot.id}`}>View Chatbot</a></p>
+        </div> 
+        {/each}
+    </div>
+</div>
+
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 50px auto;
+    }
+    .blogposts {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+
+    .post {
+        border: 1px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 0 10px #eee;
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    .readmore {
+        color: rgb(10, 10, 139);
+        text-align: right;
+        margin: 20px 0 0 0;
+    }
+
+    .readmore:hover {
+        color: green;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,140 @@
+<!-- src/routes/index.svelte -->
+<svelte:head>
+	<title>David Adeneye</title>
+</svelte:head>
+
+<script>
+import { onMount } from "svelte";
+import { OpenAI }  from "../lib/openai";
+
+  const chatbots = [
+    { id: 1, name: "Chatbot 1" },
+    { id: 2, name: "Chatbot 2" },
+    { id: 3, name: "Chatbot 3" },
+    { id: 4, name: "Chatbot 4" },
+    { id: 5, name: "Chatbot 5" },
+    { id: 6, name: "Chatbot 6" },
+  ];
+</script>
+
+<div class="container">
+	<div class="info">
+		<div class="animate-background">
+		  <div class="container info-container">
+			<div class="info-text">
+			  <h1>
+				<p class="animate-text-1">School-taught Computer Scientist,</p>
+				<p class="text-2">Self-taught Software Engineer</p>
+			  </h1>
+			</div>
+			<h2 class="subtitle">
+			  On a mission to help SMEs across the globe do businesses seamlessly and digitally.
+			</h2>
+			<div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+			  <a href="/about">See More About Me</a><div class="arrow">→</div>
+			</div>
+		  </div>
+		</div>
+	</div>
+  
+  <div id="app">
+    <form on:submit|preventDefault={handleSubmit}>
+      <label for="user-input">Ask GPT-3</label>
+      <input
+        id="user-input"
+        type="text"
+        bind:value={userInput}
+        placeholder="Type your question"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+    {#if isLoading}
+      <p>Loading...</p>
+    {:else if response}
+      <p>{response.choices[0].text.trim()}</p>
+    {/if}
+  </div>
+  
+</div>
+
+<h2>Select a Chatbot:</h2>
+
+	<ul>
+	  {#each chatbots as chatbot}
+	    <li>
+	      <a href="/chatbots/{chatbot.id}">{chatbot.name}</a>
+	    </li>
+	  {/each}
+	</ul>
+</div>
+
+<div class="background-div"></div>
+
+
+<style>
+    .info {
+        text-align: center;
+        position: relative;
+    }        
+
+    .info-container {
+        height: 100%;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+
+    .container {
+		    max-width: 1400px;
+        margin: 190px 0 0 2rem;
+    }
+
+    .info h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+		text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+
+    .info h2 {
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.50rem;
+    }
+
+	.background-div {
+		background-color: var(--background);
+    	width: 100%;
+		height: 56vh;
+		margin: -155px auto;
+	}
+    
+   
+    .info .info-text {
+        position: relative;
+    }
+
+	.scroll-button { 
+		display: flex;
+		border: none;
+		outline: none;
+		cursor: pointer;
+	}
+
+  	.scroll-button .arrow {
+  		margin-left: 0.50rem;
+	}
+
+</style>
